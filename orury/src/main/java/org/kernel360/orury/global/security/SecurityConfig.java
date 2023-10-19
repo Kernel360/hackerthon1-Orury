407440668970b2accabed666a9084cd11db70fc8
daniel <kbry12@naver.com>
2023-10-19 11:57:07 +0900

    refactor : reset SecurityConfig to bypass spring security for development purpose only

--- orury/src/main/java/org/kernel360/orury/global/security/SecurityConfig.java
+++ orury/src/main/java/org/kernel360/orury/global/security/SecurityConfig.java
@@ -2,41 +2,37 @@ package org.kernel360.orury.global.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.kernel360.orury.user.service.UserService;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
-        return http.authorizeRequests(auth -> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .mvcMatchers(
-                                HttpMethod.GET,
-                                "/"
-                        ).permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(withDefaults())
-                .logout(it -> it.logoutSuccessUrl("/"))
-                .csrf().disable()
-                .build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(
+//            HttpSecurity http
+//    ) throws Exception {
+//        return http.authorizeRequests(auth -> auth
+//                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//                        .mvcMatchers(
+//                                HttpMethod.GET,
+//                                "/"
+//                        ).permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(withDefaults())
+//                .logout(it -> it.logoutSuccessUrl("/"))
+//                .csrf().disable()
+//                .build();
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(UserService userService) {
@@ -50,4 +46,9 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
+
 }
